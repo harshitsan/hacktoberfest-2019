f1d22481ed1bb51644419506308cfa4847a93578
Ritesh Kumar <56651899+wordssaysalot@users.noreply.github.com>
2020-10-07 14:00:29 +0530

    Create basic_syntex.c

---
+++ students list/hacktoberfest-2019-master/basic_syntex.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+   printf("hi."); 
+}
+
+
+ // Details on Header Files
+//<assert.h>	    Program assertion functions
+//<ctype.h>	      Character type functions
+//<locale.h>  	  Localization functions
+//<math.h>	      Mathematics functions
+//<setjmp.h> 	    Jump functions
+//<signal.h>     	Signal handling functions
+//<stdarg.h>	    Variable arguments handling functions
+//<stdio.h>	      Standard Input/Output functions
+//<stdlib.h>     	Standard Utility functions
+//<string.h>    	String handling functions
+//<time.h>      	Date time functions
